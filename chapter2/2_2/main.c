2363a656ee46dc11be387080c51789dd46162157
MartinHan <hanhan145@163.com>
2020-03-10 13:32:49 +0800

    update

--- chapter2/2_2/main.c
+++ chapter2/2_2/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 
+#define MaxSize 50
+
+
+//ElemType
+//
+//typedef struct {
+//    ElemType date[MaxSize];
+//    int length
+//} SqList;
+
 int main() {
     printf("Hello, World!\n");
     return 0;
